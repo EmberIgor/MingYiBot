775262c24ac8421823cd01239481e7ac66fa12cf
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-02-25 01:06:00 +0800

    feat(requirements):更新requirements

--- MingYi/src/plugins/dailynews/__init__.py
+++ MingYi/src/plugins/dailynews/__init__.py
@@ -13,3 +13,5 @@ async def handle_dailyNews(event: Event, message: Message = CommandArg()):
     res = requests.get("https://api.vvhan.com/api/60s?type=json")
     url = json.loads(res.text)["imgUrl"]
     await dailyNews.send(MessageSegment.image(url))
+
+# test

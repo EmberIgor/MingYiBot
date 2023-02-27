604293d0ff60d0cbd939e9d35acfd19e44ec139d
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-02-27 17:21:25 +0800

    feat(chatGPT):黑名单

--- MingYi/src/plugins/test/__init__.py
+++ MingYi/src/plugins/test/__init__.py
@@ -3,7 +3,7 @@ from nonebot.plugin import on_notice, on_request, on_message
 
 noticeHandler = on_notice()
 requestsHandler = on_request()
-messageHandler = on_message()
+# messageHandler = on_message(block=False)
 
 
 @noticeHandler.handle()
@@ -20,9 +20,9 @@ async def handle_requestsHandler(event: Event):
     print("==============================")
 
 
-@messageHandler.handle()
-async def handle_messageHandler(event: Event):
-    print("==========on_message==========")
-    print(event.dict())
-    print(event.dict()['sender']['user_id'])
-    print("==============================")
+# @messageHandler.handle()
+# async def handle_messageHandler(event: Event):
+#     print("==========on_message==========")
+#     print(event.dict())
+#     print(event.dict()['sender']['user_id'])
+#     print("==============================")

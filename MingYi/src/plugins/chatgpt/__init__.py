2fa073e5c497aca73c9da3a1c2470b896386f2a5
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-02-28 18:06:36 +0800

    feat(voice):chatGPT接入语音回复

--- MingYi/src/plugins/chatgpt/__init__.py
+++ MingYi/src/plugins/chatgpt/__init__.py
@@ -1,15 +1,23 @@
 from nonebot import get_driver
 from nonebot.rule import to_me
+from nonebot.permission import SUPERUSER
 from nonebot.adapters import Message, Event
 from nonebot.params import CommandArg
 from nonebot.plugin import on_command
 from revChatGPT.V1 import Chatbot
+from nonebot.adapters.onebot.v11 import MessageSegment
+import voiceHandler
+import os
 
 chatbot = Chatbot(config={
     "access_token": get_driver().config.chatgpt_access_token
 })
-chatgptMessageHandler = on_command("", to_me(), priority=100)
 conversationList = chatbot.get_conversations()
+is_voice_answer = False
+
+chatgptMessageHandler = on_command("", to_me(), priority=100)
+voice_answer_on = on_command("开启语音回复", to_me(), permission=SUPERUSER)
+voice_answer_off = on_command("关闭语音回复", to_me(), permission=SUPERUSER)
 
 
 @chatgptMessageHandler.handle()
@@ -20,7 +28,7 @@ async def handle_chatgptMessage(event: Event, message: Message = CommandArg()):
     global conversationList
     conversation = None
     chatResultMessage = ""
-    character_settings = "接下来你将扮演一个叫茗懿的中国女生，用活泼可爱的语气说话。接下来是我对你说的话："
+    character_settings = "假设你是一个叫茗懿的中国女生，用活泼可爱的语气说话。"
     send_message = character_settings + str(message)
     if len(conversationList) > 0:
         conversation = conversationList[0]['id']
@@ -31,8 +39,29 @@ async def handle_chatgptMessage(event: Event, message: Message = CommandArg()):
         try:
             for data in chatbot.ask(send_message, conversation_id=str(conversation)):
                 chatResultMessage = data["message"]
-        except KeyError as key_error:
+        except KeyError:
             for data in chatbot.ask(send_message):
                 chatResultMessage = data["message"]
             conversationList = chatbot.get_conversations()
-    await chatgptMessageHandler.send(message=chatResultMessage, at_sender=True)
+    # await chatgptMessageHandler.send(message=chatResultMessage, at_sender=True)
+    if is_voice_answer:
+        ssml = voiceHandler.message_to_ssml(chatResultMessage, voice_type="cheerful")
+        waveUrl = voiceHandler.get_speech(ssml)
+        await chatgptMessageHandler.send(MessageSegment.record("file:///" + str(waveUrl).replace('\\', '/')))
+        os.remove(str(waveUrl))
+    else:
+        await chatgptMessageHandler.send(message=chatResultMessage, at_sender=True)
+
+
+@voice_answer_on.handle()
+async def handle_voice_answer_on(event: Event):
+    global is_voice_answer
+    is_voice_answer = True
+    await voice_answer_on.send("语音回复已开启")
+
+
+@voice_answer_off.handle()
+async def handle_voice_answer_off(event: Event):
+    global is_voice_answer
+    is_voice_answer = False
+    await voice_answer_off.send("语音回复已关闭")

a3a93411602c009adede2633c89799e1ad684456
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-03-04 18:00:43 +0800

    feat(chatgpt):修复群聊时空白输出的问题

--- MingYi/src/plugins/chatgpt/__init__.py
+++ MingYi/src/plugins/chatgpt/__init__.py
@@ -12,6 +12,7 @@ from nonebot.adapters.onebot.v11 import MessageSegment, Bot
 chatHandler = ChatHandler()
 is_voice_answer = False
 is_chatgpt_function_on = True
+is_debug_mode_on = False
 chat_mode = "默认"
 
 chatgptMessageHandler = on_command("", to_me(), priority=100)
@@ -20,6 +21,7 @@ voice_answer_off = on_command("关闭语音回复", to_me())
 chatgpt_function_on = on_command("开启聊天机器人", to_me(), permission=SUPERUSER)
 chatgpt_function_off = on_command("关闭聊天机器人", to_me(), permission=SUPERUSER)
 chatgpt_mode_change = on_regex("^切换到.+模式$", rule=to_me(), permission=SUPERUSER)
+debug_mode_switch = on_regex("^(^开启|^关闭)调试模式$", rule=to_me(), permission=SUPERUSER)
 
 
 @chatgpt_mode_change.handle()
@@ -38,6 +40,10 @@ async def handle_chatgptMessage(bot: Bot, event: Event, message: Message = Comma
     if not is_chatgpt_function_on:
         await chatgptMessageHandler.send("聊天功能已暂时被主人关闭")
         return
+    if is_debug_mode_on:
+        if not await SUPERUSER(bot, event):
+            await chatgptMessageHandler.send("目前主人设定为调试模式，您无法使用聊天功能", at_sender=True)
+            return
     if event.dict()['sender']['user_id'] == 1306401441 \
             or event.dict()['sender']['user_id'] == 1446534506 \
             or event.dict()['sender']['user_id'] == 2796338486:
@@ -54,10 +60,11 @@ async def handle_chatgptMessage(bot: Bot, event: Event, message: Message = Comma
     cq_message = f"[CQ:reply,id={event.dict()['message_id']}][CQ:at,qq={event.dict()['sender']['user_id']}] " \
                  f"[CQ:at,qq={event.dict()['sender']['user_id']}]" \
         if group_id is not None else ""
+    linefeed = "\n" if group_id is not None else ""
     await bot.call_api("send_msg", message_type=f"{event.dict()['message_type']}",
                        user_id=event.dict()['sender']['user_id'],
                        group_id=group_id,
-                       message=f"{cq_message}" + "\n" if group_id is not None else "" + f"{chatResultMessage}")
+                       message=f"{cq_message}" + linefeed + f"{chatResultMessage}")
 
 
 # @chatgptMessageHandler.handle()
@@ -126,3 +133,18 @@ async def handle_chatgpt_function_off(bot: Bot, event: Event):
         await chatgpt_function_off.send("聊天机器人已关闭")
     else:
         await chatgpt_function_off.send("你没有权限关闭聊天机器人", at_sender=True)
+
+
+@debug_mode_switch.handle()
+async def handle_debug_mode_switch(bot: Bot, event: Event):
+    global is_debug_mode_on
+    message = event.dict()['message']
+    mode_choose = re.findall(r"(.+?)调试模式", str(message))[0]
+    if mode_choose == "开启":
+        is_debug_mode_on = True
+    elif mode_choose == "关闭":
+        is_debug_mode_on = False
+    if await SUPERUSER(bot, event):
+        await debug_mode_switch.send(f"调试模式已{'开启' if is_debug_mode_on else '关闭'}")
+    else:
+        await debug_mode_switch.send("你没有权限开启调试模式", at_sender=True)

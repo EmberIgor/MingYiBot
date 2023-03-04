1ccc68c258259f83ac17af5ac19ee2ee2dbc0e8b
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-03-04 19:40:11 +0800

    feat(chatgpt):设置代理

--- MingYi/bot.py
+++ MingYi/bot.py
@@ -1,12 +1,16 @@
 import os
 import sys
 import nonebot
+from nonebot import get_driver
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 sys.path.append(f"{os.getcwd()}/src/tools")
 
 nonebot.init()
 
+if get_driver().config.usr_proxy == "True":
+    os.environ["http_proxy"] = get_driver().config.http_proxy
+    os.environ["https_proxy"] = get_driver().config.https_proxy
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
 

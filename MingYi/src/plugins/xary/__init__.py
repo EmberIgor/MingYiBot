f242e41acdce6631ba02be17d388685851199b5e
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-03-09 17:34:11 +0800

    feat(xrayR):改为只有好友才能索要订阅链接

--- MingYi/src/plugins/xary/__init__.py
+++ MingYi/src/plugins/xary/__init__.py
@@ -14,6 +14,11 @@ apply_for_a_subscription_link = on_command("申请订阅链接")
 
 @query_subscription_link.handle()
 async def handle_query_subscription_link(bot: Bot, event: Event, message: Message = CommandArg()):
+    friend_list = await bot.call_api("get_friend_list")
+    if event.dict()['sender']['user_id'] not in [friend['user_id'] for friend in friend_list]:
+        await apply_for_a_subscription_link.send(
+            f"你还不是茗懿的好友，请先添加茗懿为好友吧。添加好友所需的验证信息为fire-keeper", at_sender=True)
+        return
     user_id = event.dict()['sender']['user_id']
     subscription_link = get_subscription_link(user_id)
     if subscription_link["status"] == "error":
@@ -24,15 +29,17 @@ async def handle_query_subscription_link(bot: Bot, event: Event, message: Messag
         # 如果是群消息
         if event.dict()['message_type'] == "group":
             await query_subscription_link.send(f"您的订阅链接已经私聊给您", at_sender=True)
-            await bot.call_api("send_private_msg", group_id=event.dict()['group_id'],
-                               user_id=event.dict()['sender']['user_id'],
-                               message=f"您的订阅链接为：\n{subscription_link['subscription_link']}")
+            await query_subscription_link.send(f"您的订阅链接为：\n{subscription_link['subscription_link']}")
         else:
             await query_subscription_link.send(f"您的订阅链接为：\n{subscription_link['subscription_link']}")
 
 
 @apply_for_a_subscription_link.handle()
 async def handle_apply_for_a_subscription_link(bot: Bot, event: Event, message: Message = CommandArg()):
+    friend_list = await bot.call_api("get_friend_list")
+    if event.dict()['sender']['user_id'] not in [friend['user_id'] for friend in friend_list]:
+        await apply_for_a_subscription_link.send(f"你还不是茗懿的好友，请先添加茗懿为好友吧。添加好友所需的验证信息为fire-keeper", at_sender=True)
+        return
     await apply_for_a_subscription_link.send(f"正在申请订阅链接，请稍等", at_sender=True)
     user_id = event.dict()['sender']['user_id']
     apply_link = apply_subscription_link(user_id, event.dict()['sender']['nickname'])
@@ -41,8 +48,6 @@ async def handle_apply_for_a_subscription_link(bot: Bot, event: Event, message: 
     else:
         if event.dict()['message_type'] == "group":
             await apply_for_a_subscription_link.send(f"您的订阅链接已经私聊给您", at_sender=True)
-            await bot.call_api("send_private_msg", group_id=event.dict()['group_id'],
-                               user_id=event.dict()['sender']['user_id'],
-                               message=f"您的订阅链接为：\n{apply_link['subscription_link']}")
+            await apply_for_a_subscription_link.send(f"您的订阅链接为：\n{apply_link['subscription_link']}")
         else:
             await apply_for_a_subscription_link.send(f"您的订阅链接为：\n{apply_link['subscription_link']}")

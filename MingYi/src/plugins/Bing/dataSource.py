b89e38c4554dec69c138ef2350c083c2cc8839c8
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-03-29 20:24:40 +0800

    feat(bing): 添加bing机器人

--- MingYi/src/plugins/Bing/dataSource.py
+++ MingYi/src/plugins/Bing/dataSource.py
@@ -16,33 +16,43 @@ class BingHandler:
         bing_res_message = "似乎出了一些问题，无法回复。"
         is_bot_refreshed = False
         is_error = False
+        # try:
+        #     if user_id not in self.transcripts_of_conversations:
+        #         self.transcripts_of_conversations[user_id] = {
+        #             "bot": Chatbot(cookies=self.cookies),
+        #             "invocationId": '0',
+        #             "last_conversation_time": datetime.datetime.now()
+        #         }
+        #     time_diff = datetime.datetime.now() - self.transcripts_of_conversations[user_id]["last_conversation_time"]
+        #     if time_diff.total_seconds() > 600 or int(self.transcripts_of_conversations[user_id]["invocationId"]) >= 14:
+        #         self.transcripts_of_conversations[user_id]["bot"].close()
+        #         self.transcripts_of_conversations[user_id]["bot"] = Chatbot(cookies=self.cookies)
+        #     bot_response = await self.transcripts_of_conversations[user_id]["bot"].ask(
+        #         prompt=str(message),
+        #         conversation_style=ConversationStyle.balanced
+        #     )
+        #     bing_res_message = re.sub(r'\[\^.+?\^]', r'', bot_response["item"]["messages"][1]["text"])
+        #     self.transcripts_of_conversations[user_id]["invocationId"] = bot_response["invocationId"]
+        #     self.transcripts_of_conversations[user_id]["last_conversation_time"] = datetime.datetime.now()
+        # except Exception as e:
+        #     print(e)
+        #     is_error = True
+        #     bing_res_message = "似乎出了一些问题，已经重置会话，错误类型: " + str(type(e))
+        #     self.transcripts_of_conversations[user_id]["bot"].close()
+        #     del self.transcripts_of_conversations[user_id]
         try:
-            if user_id not in self.transcripts_of_conversations:
-                self.transcripts_of_conversations[user_id] = {
-                    "bot": Chatbot(cookies=self.cookies),
-                    "invocationId": '0',
-                    "last_conversation_time": datetime.datetime.now()
-                }
-            time_diff = datetime.datetime.now() - self.transcripts_of_conversations[user_id]["last_conversation_time"]
-            if time_diff.total_seconds() > 600 or int(self.transcripts_of_conversations[user_id]["invocationId"]) >= 14:
-                self.transcripts_of_conversations[user_id]["bot"].close()
-                self.transcripts_of_conversations[user_id]["bot"] = Chatbot(cookies=self.cookies)
+            bot = Chatbot(cookies=self.cookies)
             bot_response = await self.transcripts_of_conversations[user_id]["bot"].ask(
                 prompt=str(message),
                 conversation_style=ConversationStyle.balanced
             )
-            bing_res_message = re.sub(r'\[\^.+?\^]', r'', bot_response["item"]["messages"][1]["text"])
-            self.transcripts_of_conversations[user_id]["invocationId"] = bot_response["invocationId"]
-            self.transcripts_of_conversations[user_id]["last_conversation_time"] = datetime.datetime.now()
+            bot.close()
         except Exception as e:
             print(e)
             is_error = True
-            bing_res_message = "似乎出了一些问题，已经重置会话，错误类型: " + str(type(e))
-            self.transcripts_of_conversations[user_id]["bot"].close()
-            del self.transcripts_of_conversations[user_id]
         return {
             "message": bing_res_message,
             "is_bot_refreshed": is_bot_refreshed,
             "is_error": is_error,
-            "invocationId": self.transcripts_of_conversations[user_id]["invocationId"] if not is_error else None
+            # "invocationId": self.transcripts_of_conversations[user_id]["invocationId"] if not is_error else None
         }

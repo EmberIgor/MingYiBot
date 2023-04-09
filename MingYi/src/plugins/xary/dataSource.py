9a48b4861dd7ad1c94f1d2d3ad89b1383f0858b4
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-04-09 17:15:39 +0800

    feat(xrayR): xrayR更新

--- MingYi/src/plugins/xary/dataSource.py
+++ MingYi/src/plugins/xary/dataSource.py
@@ -4,19 +4,19 @@ import requests
 from nonebot import get_driver
 
 headers = {
-    "authority": "nanocore.top",
+    "authority": "firekeeper.top",
     "accept": "*/*",
     "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
     "authorization": get_driver().config.nanocore_authorization,
     "dnt": "1",
-    "referer": "https://nanocore.top/adminuser",
+    "referer": "https://firekeeper.top/adminUser",
 }
 
 
 def get_subscription_link(user_id: str):
     subscription_link = None
     try:
-        subscription_links = requests.get("https://nanocore.top/api/v1/adminuser/user/fetch?pageSize=150&current=1",
+        subscription_links = requests.get("https://firekeeper.top/api/v1/adminUser/user/fetch?pageSize=10&current=1",
                                           headers=headers).json()
     except requests.exceptions.SSLError:
         return {"status": "error", "message": "无法连接到订阅链接服务器。"}
@@ -40,26 +40,26 @@ def apply_subscription_link(user_id: str, nickname: str):
     if subscription_link["subscription_link"] is not None:
         return {"status": "error", "message": "您已经申请过订阅链接。"}
     try:
-        apply_sublink = requests.post("https://nanocore.top/api/v1/adminuser/user/generate",
+        apply_sublink = requests.post("https://firekeeper.top/api/v1/adminUser/user/generate",
                                       headers=headers,
-                                      data={"email_suffix": "nanocore.top",
+                                      data={"email_suffix": "firekeeper.top",
                                             "email_prefix": f"{nickname}",
                                             "plan_id": "1"
                                             }
                                       ).json()
         if apply_sublink["data"] == "true" or apply_sublink["data"] is True:
             subscription_info_list = requests.get(
-                "https://nanocore.top/api/v1/adminuser/user/fetch?pageSize=150&current=1",
+                "https://firekeeper.top/api/v1/adminUser/user/fetch?pageSize=150&current=1",
                 headers=headers).json()
             subscription_info = {}
             for subscription_info_json in subscription_info_list["data"]:
-                if subscription_info_json["email"] == f"{nickname}@nanocore.top":
+                if subscription_info_json["email"] == f"{nickname}@firekeeper.top":
                     subscription_info = subscription_info_json
                     break
             if subscription_info == {}:
                 return {"status": "error", "message": "申请订阅链接失败。"}
             subscription_info["remarks"] = "{" + f'"qq": "{user_id}"' + "}"
-            update_result = requests.post("https://nanocore.top/api/v1/adminuser/user/update", headers=headers,
+            update_result = requests.post("https://firekeeper.top/api/v1/adminUser/user/update", headers=headers,
                                           data=subscription_info).json()
             if update_result["data"] == "true" or update_result["data"] is True:
                 return {"status": "success", "message": "申请订阅链接成功。",

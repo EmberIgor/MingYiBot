ce0e7343b2260d2c28ec8fd64cf02a998757093d
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-06-01 17:32:17 +0800

    feat(Test): 修改test模块的输出

--- MingYi/src/plugins/test/__init__.py
+++ MingYi/src/plugins/test/__init__.py
@@ -1,9 +1,7 @@
 from nonebot.adapters import Message, Event
 from nonebot.plugin import on_notice, on_request, on_message
-import os
-from nonebot import get_bot
-from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import Bot
+import datetime
 
 noticeHandler = on_notice(block=False)
 requestsHandler = on_request(block=False)
@@ -28,4 +26,7 @@ async def handle_requestsHandler(event: Event):
 async def handle_messageHandler(bot: Bot, event: Event):
     print("==========on_message==========")
     print(event.dict())
+    print(
+        f"{datetime.datetime.fromtimestamp(event.dict()['time']).strftime('%Y-%m-%d %H:%M:%S')} "
+        f"{event.dict()['sender']['nickname']}: {event.dict()['message']}")
     print("==============================")

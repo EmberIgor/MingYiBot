46fa9c51e570f360174a66a6b73ee561e8d54209
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-06-20 23:45:12 +0800

    feat(dailynews):更新api

--- MingYi/src/plugins/dailynews/__init__.py
+++ MingYi/src/plugins/dailynews/__init__.py
@@ -12,18 +12,21 @@ dailyNews = on_command("今日新闻", block=True)
 
 @dailyNews.handle()
 async def handle_dailyNews():
-    res = requests.get("https://api.vvhan.com/api/60s?type=json")
-    url = json.loads(res.text)["imgUrl"]
-    await dailyNews.send(MessageSegment.image(url))
+    # res = requests.get("https://api.vvhan.com/api/60s?type=json")
+    # url = json.loads(res.text)["imgUrl"]
+    # await dailyNews.send(MessageSegment.image(url))
+    await dailyNews.send(MessageSegment.image("https://api.03c3.cn/zb/"))
 
 
 @scheduler.scheduled_job('cron', hour='08', minute='00')
 async def _():
     bot = get_bot()
     group_list = await bot.call_api('get_group_list')
-    res = requests.get("https://api.vvhan.com/api/60s?type=json")
-    url = json.loads(res.text)["imgUrl"]
+    # res = requests.get("https://api.vvhan.com/api/60s?type=json")
+    # url = json.loads(res.text)["imgUrl"]
     for group in group_list:
         # 如果群号不在黑名单中
         if group["group_id"] not in [984625860, 1041873822]:
-            await bot.call_api('send_group_msg', group_id=group["group_id"], message=f'[CQ:image,file={url}]')
+            # await bot.call_api('send_group_msg', group_id=group["group_id"], message=f'[CQ:image,file={url}]')
+            await bot.call_api('send_group_msg', group_id=group["group_id"],
+                               message=f'[CQ:image,file=https://api.03c3.cn/zb/]')

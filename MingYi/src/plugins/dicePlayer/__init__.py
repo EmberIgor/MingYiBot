c114b42c074b78f701a6801ce1dfcf868e6bb570
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-12-14 12:55:56 +0800

    feat(dice): 添加投骰子的功能

---
+++ MingYi/src/plugins/dicePlayer/__init__.py
@@ -0,0 +1,21 @@
+import os
+import voiceHandler
+import re
+from nonebot import on_regex, on_command
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.rule import to_me
+from nonebot.adapters import Event
+from nonebot.typing import T_State
+from .dataSource import *
+
+dice_order_pattern = r"\.?\d*rd\d+(?:/\d+)?"
+dice_player = on_regex(dice_order_pattern)
+
+
+@dice_player.handle()
+async def _(event: Event, state: T_State):
+    print(event.dict()['message'])
+    rolls, sides, threshold = dataSource.extract_dice_data(str(event.dict()['message']))
+    dice_results, total, effect = dataSource.roll_dice(rolls, sides, threshold)
+    chat_api_res_message = dataSource.chat(rolls, dice_results, total, effect)
+    await dice_player.send(chat_api_res_message, at_sender=True)

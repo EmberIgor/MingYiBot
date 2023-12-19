bee04609ccaada57150f4855330475ea5a6d7bc2
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2023-12-19 08:43:29 +0800

    feat(news): 更换接口

--- MingYi/src/plugins/dailynews/__init__.py
+++ MingYi/src/plugins/dailynews/__init__.py
@@ -22,11 +22,10 @@ async def handle_dailyNews():
 async def _():
     bot = get_bot()
     group_list = await bot.call_api('get_group_list')
-    res = requests.get("http://dwz.2xb.cn/zaob")
-    url = json.loads(res.text)["imageUrl"]
+    # res = requests.get("http://dwz.2xb.cn/zaob")
+    # url = json.loads(res.text)["imageUrl"]
+    url = "https://api.jun.la/60s.php?format=image"
     for group in group_list:
         # 如果群号不在黑名单中
         if group["group_id"] not in [984625860, 1041873822]:
             await bot.call_api('send_group_msg', group_id=group["group_id"], message=f'[CQ:image,file={url}]')
-            # await bot.call_api('send_group_msg', group_id=group["group_id"],
-            #                    message=f'[CQ:image,file=http://dwz.2xb.cn/zaob]')

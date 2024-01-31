154c13e1281893df7069f0b6030ec8a940e6062d
Emberlgor <48055760+EmberIgor@users.noreply.github.com>
2024-01-31 19:16:12 +0800

    test(test): 服务器测试

--- MingYi/src/plugins/test/__init__.py
+++ MingYi/src/plugins/test/__init__.py
@@ -1,11 +1,21 @@
 from nonebot.adapters import Message, Event
-from nonebot.plugin import on_notice, on_request, on_message
+from nonebot.plugin import on_notice, on_request, on_message, on_command
 from nonebot.adapters.onebot.v11 import Bot
 import datetime
+
+from nonebot import require, get_bot
 
 noticeHandler = on_notice(block=False)
 requestsHandler = on_request(block=False)
 messageHandler = on_message(block=False)
+test_function_handler = on_command('test')
+
+
+@test_function_handler.handle()
+async def _():
+    bot = get_bot()
+    group_list = await bot.call_api('get_group_list')
+    print(group_list)
 
 
 @noticeHandler.handle()
